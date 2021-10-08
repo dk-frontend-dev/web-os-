4e7df595e83aeea807362286422f33bc79ba0415
David Kasumov <davidkasumovFrontend@gmail.com>
2021-10-08 10:19:56 +0300

    feat: tune ngrx store & ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {StoreModule} from '@ngrx/store'
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from '@/app-routing.module'
+import {AppComponent} from '@/app.component'
+import {environment} from '@env/environment'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({
+      router: routerReducer
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 20,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
